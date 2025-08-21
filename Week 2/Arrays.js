26e718d0f859fb3fa76bf204c8ed1983c2875203
Yaro <144354667+sukyaro@users.noreply.github.com>
2025-08-21 19:35:32 +0100

    Add files via upload

---
+++ Week 2/Arrays.js
@@ -0,0 +1,18 @@
+
+// Declaring an Array
+              // 0       1        2        3       4        5
+let myArray = ["Yaro", "Hari", "Hannah", "Jason", "Mike", "Lucy"];
+console.log(myArray);
+console.log(myArray[1]); 
+
+// Adding data to the array
+myArray.push("Michael");
+console.log(myArray);
+
+// Removing data from the array
+myArray.pop()
+const newLast = myArray.pop()
+
+// Outputting the array
+console.log(myArray);
+console.log(newLast);
