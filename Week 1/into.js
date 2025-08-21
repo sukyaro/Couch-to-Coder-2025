61b76760313ce5db3444381e940ffc2989392c63
Yaro <144354667+sukyaro@users.noreply.github.com>
2025-08-21 19:34:58 +0100

    Add files via upload

---
+++ Week 1/into.js
@@ -0,0 +1,62 @@
+
+let message = "Hello people";
+const number = 25;
+
+console.log(message + " " + number)
+
+
+
+// // String: Text, single or double quoted
+// 'thats a string';
+// 'thats a string as well'
+
+// // Number: Whole numbers and fractions
+// 40
+// 34.09
+// -293
+// NaN // It is not a number but it gets returned when the value
+// // cannot be computed or as a result of attempted number coercion (type convention)
+
+// // Boolean: True or false value
+// true
+// false
+
+// // Undefined: We are getting this when were not setting a value
+// // or when we have no control over the nothingness.
+// // It worns us that we are forgetting something
+// undefined
+
+// // Null: This is an intentional missing value
+// // we sometimes need to set something to null
+// null
+
+// // Object: This a dictionary representing a collection of key-value pairs
+// {Name: 'Yaro'; age: 19;}
+
+// // Array: This represents an ordered list of values
+// [1, 2, 3, 'Apple']
+
+
+
+
+// console.log(12 === 60);
+// console.log(12 >= 12);
+
+
+
+// // AND opetation
+// &&
+
+// // OR opertion
+// ||
+
+
+const age = 18;
+if (age >= 60) {
+    console.log("Youre too old to buy sigs lol");
+} else if (age < 18) {
+    console.log("Nah mate too young yet");
+} else {
+    console.log("Yeah ther eyou go mate");
+}
+
